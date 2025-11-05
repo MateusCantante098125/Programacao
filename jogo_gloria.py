0f03407e9e0a6fdd4ef802d5021ff909d4b2eb2a
Mateus <secoalte@gmail.com>
2025-11-05 12:35:40 +0000

    update

--- jogo_gloria.py
+++ jogo_gloria.py
@@ -1,28 +1,27 @@
 import f_gloria as f
 
-def jogo():
-    menu = {
-        "1": "Novo Jogo",
-        "2": "Carregar Jogo",
-        "3": "Configurações",
-        "4": "Sair"
-    }
+menu = {
+    "1": "Novo Jogo",
+    "2": "Carregar Jogo",
+    "3": "Configurações",
+    "4": "Sair"
+}
 
-    for numero, texto in menu.items():
-        print(f"{numero}. {texto}")
+for numero, texto in menu.items():
+    print(f"{numero}. {texto}")
 
-    opcao = input("Escolha uma opção: ")
+opcao = input("Escolha uma opção: ")
 
-    if opcao == "1":
-        print("Iniciando um novo jogo da Glória...\n")
-        f.iniciar_jogo()
+if opcao == "1":
+    print("Iniciando um novo jogo da Glória...\n")
+    f.jogo()
 
-    elif opcao == "2":
-        print("Em Desenvolvimento...")
+elif opcao == "2":
+    print("Em Desenvolvimento...")
 
-    elif opcao == "3":
-        print("Em Desenvolvimento...")
+elif opcao == "3":
+    print("Em Desenvolvimento...")
 
-    elif opcao == "4":
-        print("Saindo do jogo...")
-        exit()
+elif opcao == "4":
+    print("Saindo do jogo...")
+    exit()
